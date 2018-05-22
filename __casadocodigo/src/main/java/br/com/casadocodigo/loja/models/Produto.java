3f38ad365b360baa89d7c5fdcba0500f3b7b59d3
Marco Romero <mmcromero@gmail.com>
2018-05-22 08:32:11 -0300

    subindo coisas

--- __casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ __casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,8 +1,5 @@
 package br.com.casadocodigo.loja.models;
 
-import java.util.List;
-
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,21 +10,10 @@ public class Produto {
 	
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	
 	private String titulo;
 	private String descricao;
 	private int paginas;
-	
-	@ElementCollection
-	private List<Preco> precos;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
@@ -46,16 +32,10 @@ public class Produto {
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
 	}
+
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
 	}
-	public List<Preco> getPrecos() {
-		return precos;
-	}
-	public void setPrecos(List<Preco> precos) {
-		this.precos = precos;
-	}
-	
-	
+
 }
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -1,9 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
 import java.util.Properties;
-
 import javax.persistence.EntityManagerFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -15,36 +13,37 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 public class JPAConfiguration {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 
-		@Bean
-		public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-			LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-			
-			JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-			
-			
-			DriverManagerDataSource dataSource = new DriverManagerDataSource();
-			dataSource.setUsername("root");
-			dataSource.setPassword("");
-			dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
-			dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-			
-			factoryBean.setJpaVendorAdapter(vendorAdapter);
-			
-			Properties props = new Properties();
-			props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-			props.setProperty("hibernate.show_sql", "true");
-			props.setProperty("hibernate.hbm2ddl.auto", "update");
-			factoryBean.setJpaProperties(props);
-			
-			factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
-			
-			return factoryBean;
-		}
-		
-		@Bean
-		public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
-			retur new JpaTransactionManager(emf);
-			
-		}
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter );
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+        factoryBean.setDataSource(dataSource);
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect" , "org.hibernate.dialect.MySQL5Dialect");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        factoryBean.setJpaProperties(props);
+
+        factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
+
+        return factoryBean;
+
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
 }
